6036a94735ee572c928f954fe0061cf3ef60829a
Quentin Desgrippes <desgrippes.quentin@gmail.com>
2024-09-09 17:03:12 +0200

    Adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,35 @@ require "faker"
 puts "Running the seed"
 
 User.destroy_all
+Game.destroy_all
+Meeting.destroy_all
 
-10.times do |user, index|
-  user = User.new(email: Faker::Internet.email, username: "user#{index}", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: Faker::Date.birthday(min_age: 16, max_age: 50), phone_number: Faker::PhoneNumber.phone_number)
-  user.save
+first_game = Game.new(name: "Tarot", description: "Jeu de carte qui se joue à 5", category: "cartes")
+first_game.save!
+
+second_game = Game.new(name: "Loup-garou", description: "Jeu de carte ou il faut deviner qui sont les loup-garous", category: "cartes")
+second_game.save!
+
+10.times do |index|
+
+  user = User.new(email: Faker::Internet.email, username: "user#{index}", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, date_of_birth: Faker::Date.birthday(min_age: 16, max_age: 50), phone_number: Faker::PhoneNumber.phone_number, password: 123456)
+  user.save!
+
+  meeting = Meeting.new(title: Faker::Game.title, 
+                        description: "Description du jeu, qui est vraiment génial et c'est le jeu numéro #{index}", 
+                        players_needed_min: 3, 
+                        players_needed_max: 8,
+                        location_type: "Bar",
+                        place_name: "#{index} rue de la beuverie",
+                        place_address: Faker::Address.city,
+                        latitude: 48.86,
+                        longitude: 2.33,
+                        status: ["Plein", "Disponible", "En cours", "Annulé", "Terminé"].sample,
+                        level: ["Débutant", "Intermédiaire", "Expert"].sample,
+                        date: DateTime.now,
+                        game_id: Game.all.sample.id,
+                        user_id: User.all.sample.id)
+  meeting.save!
 end
 
 puts "..end"
