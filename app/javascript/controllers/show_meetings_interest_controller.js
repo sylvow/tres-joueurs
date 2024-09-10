ba568d6f98bd2df2a1f29c89a0d284697383fbf0
redhaboukoult <redha.boukoult@gmail.com>
2024-09-10 16:54:10 +0200

    Show without submit

---
+++ app/javascript/controllers/show_meetings_interest_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="show-meetings-interest"
+export default class extends Controller {
+  static targets = ["interested", "friends"]
+
+  connect() {
+    console.log("test hello")
+  }
+
+  display() {
+    
+    if ( this.interestedTarget.innerText === "Je suis intéressé.e" ) {
+      this.interestedTarget.innerText = "Je ne suis plus intéressé.e";
+    } else {
+      this.interestedTarget.innerText = "Je suis intéressé.e";
+    }
+    this.friendsTarget.classList.toggle("d-none")
+  }
+}
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ApplicationRecord
+  enum status: { interested: 0, accepted: 1, rejected: 2 }
+
   belongs_to :meeting
   belongs_to :user
 end
