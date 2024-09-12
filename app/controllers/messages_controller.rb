2f7be30745cc0011a773ca7ef1e10b3ec1416b57
Sylvain Dubois <sylvain.dubois12@gmail.com>
2024-09-12 12:39:17 +0200

    chats ok

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,25 @@
 class MessagesController < ApplicationController
 
   def create
-
+    @meeting = Meeting.find(params[:meeting_id])
+    @message = Message.new(message_params)
+    @message.meeting = @meeting
+    @message.user = current_user
+    if @message.save
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append(:messages, partial: "messages/message",
+            locals: { message: @message, user: current_user })
+        end
+        format.html { redirect_to meeting_messages_path(@meeting) }
+      end    else
+      render "meeting_messages", status: :unprocessable_entity
+    end
   end
 
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
