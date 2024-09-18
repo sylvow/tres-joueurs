1e1d679a06b1bc31a390b178002ecdff525eb53f
redhaboukoult <redha.boukoult@gmail.com>
2024-09-18 20:22:03 +0200

    Message CSS

--- db/migrate/20240918133345_add_viewed_requests_count_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddViewedRequestsCountToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :viewed_requests_count, :integer
-  end
-end
--- db/migrate/20240918140116_add_status_to_notification.rb
+++ db/migrate/20240918140116_add_status_to_notification.rb
@@ -1,5 +1,5 @@
 class AddStatusToNotification < ActiveRecord::Migration[7.1]
   def change
-    add_column :notifications, :status, :integer
+    add_column :notifications, :status, :integer    
   end
 end
