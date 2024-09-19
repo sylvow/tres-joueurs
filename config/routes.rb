0f8727a0bba65d7bdc4593f7fec331829e3ae187
redhaboukoult <redha.boukoult@gmail.com>
2024-09-19 13:12:20 +0200

    Home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root to: "meetings#index"
+  root to: "pages#home"
 
   resources :meetings, except: [:destroy] do
     member do
