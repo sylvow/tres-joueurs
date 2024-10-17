90352b3c5872918f5b87c6e393a8224799d71d60
Sylvain Dubois <sylvain.dubois12@gmail.com>
2024-10-17 17:13:48 +0200

    internally-stored json ok

--- app/javascript/controllers/cities_controller.js
+++ app/javascript/controllers/cities_controller.js
@@ -4,28 +4,38 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["aroundMeField", "cityInput", "results", "city", "fieldLat", "fieldLng", "fieldRadius", "toggler"]
 
-  reveal() {
-    this.cityInputTarget.classList.toggle("d-none")
-    this.aroundMeFieldTarget.classList.toggle("d-none")
+  displaySuggestions(cities) {
+    if (cities.length > 0) {
+      this.suggestionsTarget.style.display = 'block'
+      cities.forEach(city => {
+        const li = document.createElement('li')
+        li.textContent = `${city.city} (${city.postcode})`
+        li.dataset.coordinates = JSON.stringify(city.coordinates)
+        li.addEventListener('click', () => this.selectCity(city))
+        this.suggestionsTarget.appendChild(li)
+      })
+    } else {
+      this.suggestionsTarget.style.display = 'none'
+    }
   }
 
-  displayCities() {
-    const word = this.cityInputTarget.value
-    const url = `https://geo.api.gouv.fr/communes?nom=${word}&fields=departement&boost=population&limit=5`
-    fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      const results = data.map((city) => city.nom + ', ' + city.code)
-      // const lng = data.map((city) => city.centre.coordinates[1])
-      // console.log(lng)
-      console.log(results)
-      this.resultsTarget.innerHTML = ""
-      results.forEach((suggestion) => {
-        this.resultsTarget.insertAdjacentHTML('beforeend', `<li data-cities-target="city" data-action="click->cities#selectCity">${suggestion}</li>`);
-      });
-      // document.querySelector("#results").insertAdjacentHTML("afterbegin", liContent);
-    });
-  }
+  // displayCities() {
+  //   const word = this.cityInputTarget.value
+  //   const url = `https://geo.api.gouv.fr/communes?nom=${word}&fields=departement&boost=population&limit=5`
+  //   fetch(url)
+  //   .then(response => response.json())
+  //   .then((data) => {
+  //     const results = data.map((city) => city.nom + ', ' + city.code)
+  //     // const lng = data.map((city) => city.centre.coordinates[1])
+  //     // console.log(lng)
+  //     console.log(results)
+  //     this.resultsTarget.innerHTML = ""
+  //     results.forEach((suggestion) => {
+  //       this.resultsTarget.insertAdjacentHTML('beforeend', `<li data-cities-target="city" data-action="click->cities#selectCity">${suggestion}</li>`);
+  //     });
+  //     // document.querySelector("#results").insertAdjacentHTML("afterbegin", liContent);
+  //   });
+  // }
 
   selectCity(event) {
     this.cityInputTarget.value = event.target.innerText
